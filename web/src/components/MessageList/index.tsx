fd5451181688208c682ee5450b5d09697d6b9ce0
Gabriel Marioto <camilomarioto@hotmail.com>
2021-10-20 02:03:23 -0300

    finalizado frontend da aplicação

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,7 +1,8 @@
-import styles from "./styles.module.scss";
-import logoImg from "../../assets/logo.svg";
 import { useEffect, useState } from "react";
 import { api } from "../../services/api";
+import io from "socket.io-client";
+import logoImg from "../../assets/logo.svg";
+import styles from "./styles.module.scss";
 
 type Message = {
   id: string;
@@ -12,8 +13,28 @@ type Message = {
   };
 };
 
+let messagesQueue: Message[] = [];
+
+const socket = io("http://localhost:4000");
+
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     api.get<Message[]>("messages/last3").then((response) => {
