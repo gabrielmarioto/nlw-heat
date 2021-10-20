793caf1d8db02563c957f48c944d8c47fb2770b3
Gabriel Marioto <camilomarioto@hotmail.com>
2021-10-19 22:11:40 -0300

    estilizado o message list

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,14 +1,14 @@
-import styles from "./styles.module.scss";
 import { VscGithubInverted } from "react-icons/vsc";
+import styles from "./styles.module.scss";
 
 export function LoginBox() {
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
 
-      <a href="#" className="styles.signInWithGithub">
-        Entrar com github
+      <a href="#" className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
+        Entrar com github
       </a>
     </div>
   );
